1d448b4580967ce0050726fb57302c78eb987699
Phan Huy Hoang <hoangph271@gmail.com>
2022-06-17 15:32:15 +0700

    [Add] BlogLayout

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,10 +1,13 @@
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Set } from '@redwoodjs/router'
+import BlogLayout from './layouts/BlogLayout/BlogLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/about" page={AboutPage} name="about" />
-      <Route path="/" page={HomePage} name="home" />
+      <Set wrap={BlogLayout}>
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,28 +1,16 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
       <main>
         <h1>HomePage</h1>
         <p>
           Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
         </p>
         <p>
-          My default route is named <code>home</code>, link to me with `
-          <Link to={routes.home()}>Home</Link>`
+          My default route is named <code>home</code>
         </p>
       </main>
     </>
